b271fde34630437ab7855764772df99eba4356ea
reGrach <germangrach.94@yandex.ru>
2020-01-07 18:08:00 +0300

    fix url

--- lumos-front/src/axios/index.js
+++ lumos-front/src/axios/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const instance = axios.create({
-    baseURL: 'http://127.0.0.1:5000/api',
+    baseURL: 'http://127.0.0.1:8080/api',
     // baseURL: 'http://130.193.36.217:8080/api',
     timeout: 10000
 });
